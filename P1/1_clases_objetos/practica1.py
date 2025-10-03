6b74241796f98fd31ee9503c4058b6d50f0676a5
GomezAguirreLi <eli_3141240118@utd.edu.mx>
2025-10-02 21:16:04 -0600

    practica1

--- P1/1_clases_objetos/practica1.py
+++ P1/1_clases_objetos/practica1.py
@@ -1,4 +1,26 @@
 '''
-Pracca
+Practica #1.- Implementas paradigmas Estructurados vs POO
 
 '''
+
+#Estructurada
+def area_rectangulo(base,altura):
+    return base*altura
+
+print(area_rectangulo(5,3))
+
+#Orientada a Objetos
+
+
+class Rectangulo:
+    def __init__(self,base,altura):
+        self.base=base
+        self.altura=altura
+
+    def area(self):
+        return self.base*self.altura
+
+
+
+rect=Rectangulo(5,3)    
+print(rect.area())
